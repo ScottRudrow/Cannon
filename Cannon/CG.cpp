7815bb4877c286d14d10163ae9d35d7c8916c61c
Scott Rudrow <serudrow@alumni.fullsail.edu>
2024-12-16 16:09:32 -0700

    Corrected LINK errors return main error remains

---
+++ Cannon/CG.cpp
@@ -0,0 +1,31 @@
+#include "pch.h"
+#include "SDL.h"
+#include "SDL_main.h"
+#include "Form1.h"
+#include "Form2.h"
+#include "windows.h"
+#include <iostream>
+
+// Cannon Game = CG
+	
+int main(int argc, char** argv)
+{
+
+
+
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+	{
+
+		Application::EnableVisualStyles();
+		Application::SetCompatibleTextRenderingDefault(false);
+		System::Void Form1_Load(System::Object ^ sender, System::EventArgs ^ e); {
+		};
+	}
+
+}
--- Cannon/Form1.h
+++ Cannon/Form1.h
@@ -1,8 +1,8 @@
+
 #pragma once
 
-	using namespace System::CppCLRWinFormsProject; {
-
-};
+namespace Cannon
+{
 	using namespace System;
 	using namespace System::ComponentModel;
 	using namespace System::Collections;
@@ -10,12 +10,24 @@
 	using namespace System::Data;
 	using namespace System::Drawing;
 
+
+
 	/// <summary>
 	/// Summary for Form1
 	/// </summary>
 	public ref class Form1 : public System::Windows::Forms::Form
 	{
+	private:
+
+
+
 	public:
+
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
 		Form1(void)
 		{
 			InitializeComponent();
@@ -24,17 +36,7 @@
 			//
 		}
 
-	protected:
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
-		~Form1()
-		{
-			if (components)
-			{
-				delete components;
-			}
-		}
+
 	private: System::Windows::Forms::Label^ label1;
 	private: System::Windows::Forms::TextBox^ textBox1;
 	private: System::Windows::Forms::TableLayoutPanel^ tableLayoutPanel1;
@@ -72,9 +74,10 @@
 			this->label1->AutoSize = true;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Comic Sans MS", 20, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label1->Location = System::Drawing::Point(12, 59);
+			this->label1->Location = System::Drawing::Point(8, 38);
+			this->label1->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(320, 55);
+			this->label1->Size = System::Drawing::Size(218, 38);
 			this->label1->TabIndex = 0;
 			this->label1->Text = L"A Cannon Game";
 			this->label1->Click += gcnew System::EventHandler(this, &Form1::label1_Click);
@@ -85,9 +88,10 @@
 				| System::Windows::Forms::AnchorStyles::Right));
 			this->textBox1->Font = (gcnew System::Drawing::Font(L"Comic Sans MS", 16, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
 				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
-			this->textBox1->Location = System::Drawing::Point(13, 158);
+			this->textBox1->Location = System::Drawing::Point(9, 103);
+			this->textBox1->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(1178, 52);
+			this->textBox1->Size = System::Drawing::Size(787, 37);
 			this->textBox1->TabIndex = 1;
 			this->textBox1->Text = L"Name";
 			// 
@@ -100,30 +104,33 @@
 			this->tableLayoutPanel1->Controls->Add(this->button1, 0, 0);
 			this->tableLayoutPanel1->Controls->Add(this->button3, 0, 2);
 			this->tableLayoutPanel1->Controls->Add(this->button2, 0, 1);
-			this->tableLayoutPanel1->Location = System::Drawing::Point(459, 364);
+			this->tableLayoutPanel1->Location = System::Drawing::Point(306, 237);
+			this->tableLayoutPanel1->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->tableLayoutPanel1->Name = L"tableLayoutPanel1";
 			this->tableLayoutPanel1->RowCount = 3;
 			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 33.33333F)));
 			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 33.33333F)));
 			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 33.33333F)));
-			this->tableLayoutPanel1->Size = System::Drawing::Size(271, 230);
+			this->tableLayoutPanel1->Size = System::Drawing::Size(181, 150);
 			this->tableLayoutPanel1->TabIndex = 2;
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(3, 3);
+			this->button1->Location = System::Drawing::Point(2, 2);
+			this->button1->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(265, 70);
+			this->button1->Size = System::Drawing::Size(177, 45);
 			this->button1->TabIndex = 0;
 			this->button1->Text = L"PLAY";
 			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &Form1::Button1);
+			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
 			// 
 			// button3
 			// 
-			this->button3->Location = System::Drawing::Point(3, 155);
+			this->button3->Location = System::Drawing::Point(2, 100);
+			this->button3->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->button3->Name = L"button3";
-			this->button3->Size = System::Drawing::Size(265, 72);
+			this->button3->Size = System::Drawing::Size(177, 47);
 			this->button3->TabIndex = 2;
 			this->button3->Text = L"EXIT";
 			this->button3->UseVisualStyleBackColor = true;
@@ -131,21 +138,24 @@
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(3, 79);
+			this->button2->Location = System::Drawing::Point(2, 51);
+			this->button2->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(265, 70);
+			this->button2->Size = System::Drawing::Size(177, 45);
 			this->button2->TabIndex = 1;
 			this->button2->Text = L"OPTIONS";
 			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &Form1::button2_Click);
 			// 
 			// Form1
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1203, 761);
+			this->ClientSize = System::Drawing::Size(802, 495);
 			this->Controls->Add(this->tableLayoutPanel1);
 			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->label1);
+			this->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->Name = L"Form1";
 			this->Text = L"A Cannon Game";
 			this->tableLayoutPanel1->ResumeLayout(false);
@@ -154,14 +164,21 @@
 
 		}
 #pragma endregion
-	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
-	}
-	private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
-		this->Close();
-	}
-	private: System::Void Button1(System::Object^ sender, System::EventArgs^ e) {
-}
-	private: System::Void button2(System::Object^ sender, System::EventArgs^ e) {
+
+		private: System::Void label1_Click(System::Object ^ sender, System::EventArgs ^ e) {
+		}
+		private: System::Void button3_Click(System::Object ^ sender, System::EventArgs ^ e) {
+			this->Close();
+		}
+		private: System::Void button1_Click(System::Object ^ sender, System::EventArgs ^ e) {
+		}
+		private: System::Void button2_Click(System::Object ^ sender, System::EventArgs ^ e) {
+			System::Void Form2_Load(System::Object ^ sender, System::EventArgs ^ e); {
+			};
+		};
+
+
 		
 	};
-};
+}
+
